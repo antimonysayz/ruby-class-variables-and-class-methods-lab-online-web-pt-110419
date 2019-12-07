04253b8b80f67b16764c1ff854f75912b7ed8907
Anthony Mai <anthony.mai530@gmail.com>
2019-12-07 05:03:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,7 +26,8 @@ class Song
     genre_count = {}
     
     @@genres.each do |genre|
-      if genre_count[genre] += 1
+      if genre_count[genre] 
+        genre_count[genre] += 1
         
       else 
         genre_count[genre] = 1
